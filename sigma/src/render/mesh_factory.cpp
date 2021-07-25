de75cd647b17e1dcfb520d1e522160dd4c845e1c
Константин Бедрицкий <kmb2@tpu.ru>
2021-07-25 19:16:45 +0600

    Mesh factory cube creation

--- sigma/src/render/mesh_factory.cpp
+++ sigma/src/render/mesh_factory.cpp
@@ -19,14 +19,56 @@ MeshHandle MeshFactory::createQuad() {
 }
 
 MeshHandle MeshFactory::createCube() {
-  std::vector<SimpleVertex> vertices = {};
-  std::vector<uint32_t> indices = {};
+  std::vector<uint32_t> indices = {
+      0, 1, 2, 2, 3, 0,
+      4, 5, 6, 6, 7, 4,
+      8, 9, 10, 10, 11, 8,
+      12, 13, 14, 14, 15, 12,
+      16, 17, 18, 18, 19, 16,
+      20, 21, 22, 22, 23, 20,
+  };
+  std::vector<SimpleVertex> vertices = {
+      {{-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}},
+      {{0.5f, -0.5f, -0.5f}, {1.0f, 0.0f}},
+      {{0.5f, 0.5f, -0.5f}, {1.0f, 1.0f}},
+      {{-0.5f, 0.5f, -0.5f}, {0.0f, 1.0f}},
+
+      {{-0.5f, -0.5f, 0.5f}, {0.0f, 0.0f}},
+      {{0.5f, -0.5f, 0.5f}, {1.0f, 0.0f}},
+      {{0.5f, 0.5f, 0.5f}, {1.0f, 1.0f}},
+      {{-0.5f, 0.5f, 0.5f}, {0.0f, 1.0f}},
+
+      {{-0.5f, 0.5f, 0.5f}, {1.0f, 0.0f}},
+      {{-0.5f, 0.5f, -0.5f}, {1.0f, 1.0f}},
+      {{-0.5f, -0.5f, -0.5f}, {0.0f, 1.0f}},
+      {{-0.5f, -0.5f, 0.5f}, {0.0f, 0.0f}},
+
+      {{0.5f, 0.5f, 0.5f}, {1.0f, 0.0f}},
+      {{0.5f, 0.5f, -0.5f}, {1.0f, 1.0f}},
+      {{0.5f, -0.5f, -0.5f}, {0.0f, 1.0f}},
+      {{0.5f, -0.5f, 0.5f}, {0.0f, 0.0f}},
+
+      {{-0.5f, -0.5f, -0.5f}, {0.0f, 1.0f}},
+      {{0.5f, -0.5f, -0.5f}, {1.0f, 1.0f}},
+      {{0.5f, -0.5f, 0.5f}, {1.0f, 0.0f}},
+      {{-0.5f, -0.5f, 0.5f}, {0.0f, 0.0f}},
+
+      {{-0.5f, 0.5f, -0.5f}, {0.0f, 1.0f}},
+      {{0.5f, 0.5f, -0.5f}, {1.0f, 1.0f}},
+      {{0.5f, 0.5f, 0.5f}, {1.0f, 0.0f}},
+      {{-0.5f, 0.5f, 0.5f}, {0.0f, 0.0f}},
+  };
 
   MeshHandle mesh = createRef<Mesh>(PrimitiveTopology::TRIANGLE, indices.size());
-  mesh->addVertexBuffer(VertexBuffer(vertices, SimpleVertex::getLayout()));
-  mesh->setIndexBuffer(IndexBuffer(indices));
+  mesh->
+      addVertexBuffer(VertexBuffer(vertices, SimpleVertex::getLayout())
+  );
+  mesh->
+      setIndexBuffer(IndexBuffer(indices)
+  );
 
-  return mesh;
+  return
+      mesh;
 }
 
 }
